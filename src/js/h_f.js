77397fcbe982ff9894d1fb741a26403060ae3516
Rezaa <rezaa91@hotmail.co.uk>
2018-04-14 13:51:37 +0100

    made navbar effects begin when user scrolls past 0px vertical height

--- src/js/h_f.js
+++ src/js/h_f.js
@@ -2,7 +2,7 @@ $(function(){
     //apply background color to navbar when user begins to scroll
     $(window).scroll(function(){
         let scroll = $(window).scrollTop(); //find no. of pixels from top of screen
-        if(scroll>50){ //when number of pixels from top exceeds 50 apply change
+        if(scroll>0){ //when number of pixels from top exceeds 0 apply change
             $('nav').addClass('white');
             $('.navbar-brand').addClass('brand');
         }else{ //if user returns to top of page - remove classes which applied change
@@ -13,14 +13,14 @@ $(function(){
     
     //add mouse over effects to nav bar when at top of the page
     $('nav').on('mouseover', function(){
-        if($(window).scrollTop() <= 50){ //if window is at top of page - i.e. navbar hasnt already got navbar effect on
+        if($(window).scrollTop() <= 0){ //if window is at top of page - i.e. navbar hasnt already got navbar effect on
             $('nav').addClass('white');
             $('.navbar-brand').addClass('brand');
         }
     })
     //same as above but in reverse
     $('nav').on('mouseout', function(){
-        if($(window).scrollTop() <= 50){
+        if($(window).scrollTop() <= 0){
             $('nav').removeClass('white');
             $('.navbar-brand').removeClass('brand');
         }

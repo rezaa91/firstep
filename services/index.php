17c85d2f0d30062c2410e00da3991ef66e6ff000
Rezaa <rezaa91@hotmail.co.uk>
2018-04-14 13:50:54 +0100

    included modal module in to markup

--- services/index.php
+++ services/index.php
@@ -1,12 +1,17 @@
 <!--services-->
 
 <?php
-$css = "../src/css/index.css";
+$css = "../src/css/services.css";
 $page_title = "services";
 include_once('../includes/header.html');
 include_once('../includes/navigation.html');
 ?>
 
+<div id="wrapper">
+    <!--modal-->
+    <?php include_once('../includes/modal.html'); ?>
+    
+</div>
 
 
 <?php

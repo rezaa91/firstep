9657093669aa1e98dbb2e79fdf2400a2590c4a62
Rezaa <rezaa91@hotmail.co.uk>
2018-04-21 20:55:37 +0100

    added 'who we are' section

--- about-us/index.php
+++ about-us/index.php
@@ -2,6 +2,7 @@
 
 <?php
 $css = "../src/css/about-us.css";
+$js = '../src/js/about-us.js';
 $page_title = "about us";
 include_once('../includes/header.html');
 include_once('../includes/navigation.html');
@@ -21,19 +22,61 @@ include_once('../includes/navigation.html');
     </div><!--end of header-->
     
     
+    
+    <!--who are we-->
+    <div class="container" id="who">
+        
+        <h4 class="text-muted">- WHO WE ARE -</h4>
+        
+        <div class="row">
+            
+            <div class="col-md-4">
+                <div class="who-title">
+                    <h4>Our Passion</h4>
+                </div>
+                <div class="who-desc">
+                    <p>To help you excel in business</p>
+                </div>
+            </div><!--end of col 1-->
+            
+            <div class="col-md-4">
+                <div class="who-title">
+                    <h4>Our Goals</h4>
+                </div>
+                <div class="who-desc">
+                    <p>To provide you with a stunning website which will aim to improve your business</p>
+                </div>
+            </div><!--end of col 2-->
+            
+            <div class="col-md-4">
+                <div class="who-title">
+                    <h4>Our Results</h4>
+                </div>
+                <div class="who-desc">
+                    <p>Stunning websites. Increased traffic. Increased sales.</p>
+                </div>
+            </div><!--end of col 3-->
+            
+        </div><!--end of row-->
+        
+    </div><!--end of who we are section-->
+    
+    
+    
+    
     <!--images-->
     <div class="container-fluid" id="image-banner">
         <div class="row">
-            <div class="col-md-3">
+            <div class="col-md-3 banner-img">
                 <img class="img-fluid" src="../src/img/firstep-4.jpg" />
             </div><!--end of col 1-->
-            <div class="col-md-3">
+            <div class="col-md-3 banner-img">
                 <img class="img-fluid" src="../src/img/firstep-3.jpg" />
             </div><!--end of col 2-->
-            <div class="col-md-3">
+            <div class="col-md-3 banner-img">
                 <img class="img-fluid" src="../src/img/firstep-4.jpg" />
             </div><!--end of col 3-->
-            <div class="col-md-3">
+            <div class="col-md-3 banner-img">
                 <img class="img-fluid" src="../src/img/firstep-3.jpg" />
             </div><!--end of col 4-->
         </div><!--end of row-->
@@ -43,18 +86,18 @@ include_once('../includes/navigation.html');
     <!--desc-->
     <div class="container" id="about-desc">
         <div class="row" id="about-desc-banner">
-            <div class="col-md-4">
+            <div class="col-md-4 about-section">
                 <img class="img-fluid" src="../src/img/firstep-2.jpg"/>
             </div><!--end of col 1-->
             <div class="col-md-8">
-                <div class="about-desc-header">
+                <div class="about-desc-header about-section">
                     <h4 class="mission-title"><b>OUR MISSION</b></h4>
                 </div>
-                <div class="about-desc-body">
+                <div class="about-desc-body about-section">
                     <p class="lead text-muted">"Here at <span class="mission-title">FIRSTEP</span>, our mission is to create websites fit for purpose. We don't believe in a 'one size fits all' approach which is why we build all our websites from scratch. This means that we do not follow a template-styled theme like a lot of websites out there. Our templates are created with you in mind, this allows us freedom to create whatever you want. The options are limitless!"</p>
                     <p class="text-info"></p>
                 </div>
-                <div class="about-desc-footer">
+                <div class="about-desc-footer about-section">
                     <blockquote class="text-right">- Ali Issaee, <small>Managing Director</small></blockquote>
                 </div>
             </div><!--end of col 2-->

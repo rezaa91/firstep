1ee213539a67c1a6831638de2638525a71137bd2
Rezaa <rezaa91@hotmail.co.uk>
2018-04-28 11:11:12 +0100

    cleaned up

--- about.php
+++ about.php
@@ -4,6 +4,7 @@
 $css = "src/css/about-us.css";
 $js = 'src/js/about-us.js';
 $page_title = "about us";
+$modal = true;
 
 require_once 'core/init.php';
 require_once 'includes/handle-modal.php';
@@ -23,41 +24,41 @@ require_once 'includes/handle-modal.php';
     
     
     <!--who are we-->
-    <div class="container" id="who">
-        
-        <h4 class="text-muted" id="who-header">WHO ARE WE?</h4>
-        
-        <div class="row">
-            
-            <div class="col-md-4 who-section">
-                <div class="who-title">
-                    <h4>Our Passion <i class="far fa-smile"></i></h4>
-                </div>
-                <div class="who-desc">
-                    <p class="text-muted">We love creating websites! There is something special about taking an idea, or a design, and programming that idea in to a fully functioning, beautiful website.</p>
-                </div>
-            </div><!--end of col 1-->
-            
-            <div class="col-md-4 who-section">
-                <div class="who-title">
-                    <h4>Our Goals <i class="fas fa-clipboard-list"></i></h4>
-                </div>
-                <div class="who-desc">
-                    <p class="text-muted">We thrive for customer satisfaction, and that is our primary goal we set out to achieve. Until this goal is met, we will not stop working! We hope that if you are fully satisfied with the service you received, you are more likely to recommend more custom our way if the opportunity presents itself.</p>
-                </div>
-            </div><!--end of col 2-->
-            
-            <div class="col-md-4 who-section">
-                <div class="who-title">
-                    <h4>Our Results <i class="fas fa-vials"></i></h4>
-                </div>
-                <div class="who-desc">
-                    <p class="text-muted">Our current clients have reported an increase in traffic &amp; sales, which have aided to their financial goals. So far we have achieved a 100% satisfaction score from our clients which we hope will continue as we move forward.</p>
-                </div>
-            </div><!--end of col 3-->
-            
-        </div><!--end of row-->
-        
+    <div class="container-fluid" id="who">
+        <div class="container">
+            <h4 class="text-muted" id="who-header">WHO ARE WE?</h4>
+
+            <div class="row">
+
+                <div class="col-md-4 who-section">
+                    <div class="who-title">
+                        <h4>Our Passion <i class="far fa-smile"></i></h4>
+                    </div>
+                    <div class="who-desc">
+                        <p class="text-muted">We love creating websites! There is something special about taking an idea, or a design, and programming that idea in to a fully functioning, beautiful website.</p>
+                    </div>
+                </div><!--end of col 1-->
+
+                <div class="col-md-4 who-section">
+                    <div class="who-title">
+                        <h4>Our Goals <i class="fas fa-clipboard-list"></i></h4>
+                    </div>
+                    <div class="who-desc">
+                        <p class="text-muted">We thrive for customer satisfaction, and that is our primary goal we set out to achieve. Until this goal is met, we will not stop working! We hope that if you are fully satisfied with the service you received, you are more likely to recommend more custom our way if the opportunity presents itself.</p>
+                    </div>
+                </div><!--end of col 2-->
+
+                <div class="col-md-4 who-section">
+                    <div class="who-title">
+                        <h4>Our Results <i class="fas fa-vials"></i></h4>
+                    </div>
+                    <div class="who-desc">
+                        <p class="text-muted">Our current clients have reported an increase in traffic &amp; sales, which have aided to their financial goals. So far we have achieved a 100% satisfaction score from our clients which we hope will continue as we move forward.</p>
+                    </div>
+                </div><!--end of col 3-->
+
+            </div><!--end of row-->
+        </div>
     </div><!--end of who we are section-->
     
     

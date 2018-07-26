4074644b973b731d7880c82d917a5fcb0ad774a1
rezaa91 <rezaa91@hotmail.co.uk>
2018-07-26 20:54:56 +0100

    added SMTP settings

--- core/init.php
+++ core/init.php
@@ -19,6 +19,20 @@ function class_loader($class){
 
 spl_autoload_register('class_loader');
 
+
+//Email settings
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Charset = "UTF-8";
+
+$mail->Host = "smtp.mailtrap.io";
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->Port = 25;
+$mail->Username = "fe9884b4bcaf9c";
+$mail->Password = " 9a7269bd98a964";
+
+
 //include templates on every page
 require_once 'includes/header.html';
 require_once 'includes/navigation.html';
